385cd1dcb0cd303f69668afaff28e88a8a8ead1e
Aayush <aayushgautam2502@gmail.com>
2024-04-13 03:29:19 +0530

    Update: add hooks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DropdownPage from "./Pages/DropdownPage"
 import ButtonPage from "./Pages/ButtonPage"
 import ModalPage from "./Pages/ModalPage";
 import TablePage from "./Pages/TablePage";
+import CounterPage from "./Pages/CounterPage";
 
 function App(){
     return <div className="container mx-auto grid grid-cols-6 gap-4 mt-4" >
@@ -24,6 +25,9 @@ function App(){
             </Route>
             <Route path = "/table">
                 <TablePage />
+            </Route>
+            <Route path = "/counter">
+                <CounterPage initialCount={10}/>
             </Route>
         </div>
     </div>
